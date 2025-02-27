66983808be1c7a3d5c4476ff1d4a77b0977403af
Pablo080601 <pabloruiz080601@gmail.com>
2025-02-27 19:24:17 +0100

    temperaturas

---
+++ 4_arrays/4_temperaturas.php
@@ -0,0 +1,35 @@
+<?php
+
+/* Dado un array con la temperatura media de  cada día de la semana, sacar:
+- la temperatura máxima, 
+- la mínima 
+- y la temperatura media*/
+
+
+$temperaturas = [12, 15, 13, 12, 16, 11, 13];
+
+$maxima = $temperaturas[0];
+foreach ($temperaturas as $value) {
+    if ($value >= $maxima) {
+        $maxima = $value;
+    }
+}
+echo "$maxima";
+
+echo "<br>";
+
+$minima = $temperaturas[0];
+foreach ($temperaturas as $value) {
+    if ($value <= $minima) {
+        $minima = $value;
+    }
+}
+echo "$minima";
+
+echo "<br>";
+
+$media = 0;
+foreach ($temperaturas as $value) {
+    $media += $value;
+}
+echo $media / count($temperaturas);
