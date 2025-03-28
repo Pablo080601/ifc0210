8defbd04d1fee63b718c166fd0ecbce486b85fa1
Pablo080601 <pabloruiz080601@gmail.com>
2025-03-28 15:20:44 +0100

    Obtener datos mediante la url con un get

---
+++ 6_formularios/4_get_params/index.php
@@ -0,0 +1,21 @@
+<?php
+/* 
+
+Crear un programa en php que reciba dos parametros por la URL num1 y num2
+en caso de que no se especifiquen, el programa mostrará el texto: "por favor, indique los parámetros correspondientes"
+y en caso de que se especifiquen mostrar la suma de ambos números utilizando una función que devuelva: "la suma es: X"
+
+
+Pista: la función isset($variable), nos permite comprobar si una variable está definida
+*/
+
+function suma($num1, $num2)
+{
+    return $num1 + $num2;
+}
+
+if (isset($_GET["num1"]) && isset($_GET["num2"])) {
+    echo "la suma es: " . suma($_GET["num1"], $_GET["num2"]);
+} else {
+    echo "por favor, indique los parámetros correspondientes";
+}
